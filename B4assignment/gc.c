34350e13d8ab908fd17e6b0efddd57f1e7b74708
Shuhei Yoshida <aneki525@gmail.com>
2022-04-06 18:53:32 +0900

    add output method

--- B4assignment/gc.c
+++ B4assignment/gc.c
@@ -9,15 +9,30 @@ typedef double Matrix[N][N];
 
 void cg(Matrix A, Matrix b, double epsilon) {
     // https://www.kz.tsukuba.ac.jp/~abe/ohp-cfd/cfd03_tanaka_H28.pdf
-    
+
 }
 
+void output_matrix(Matrix A) {
+    for (int m = 0; m < N; m++) {
+        printf(" ");
+        for (int n = 0; n < N; n++) {
+            printf(" %f", A[m][n]);
+        }
+        printf("\n");
+    }
+}
+
+void output_vector(Vector b) {
+    for (size_t n = 0; n < N; n++) {
+        printf(" %f", b[n]);
+    }
+}
 
 
 int main(void) {
     // http://www-section.cocolog-nifty.com/blog/2008/12/cg-f85b.html
     Matrix A = {{2, 4, 0, 0}, {4, 8, 2, 2}, {0, 2, 1, 0}, {0, 2, 0, 4}};
-    Vector b = {{2}, {1}, {4}, {5}}
-    cg();
+    Vector b = {2, 1, 4, 5};
+    output_matrix(A);
     return 0;
 }
